362f770e37c0291bc5748826c7cddeef14968e68
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-14 00:04:43 +0600

    add bycivcle and train for the abstract class

---
+++ src/main/java/vehicle/Bicycle.java
@@ -0,0 +1,13 @@
+package vehicle;
+
+public class Bicycle extends TransportVehicle{
+
+    public Bicycle(String name, int speed) {
+        super(name, speed);
+    }
+
+    @Override
+    public void move() {
+        System.out.println(name + "");
+    }
+}
