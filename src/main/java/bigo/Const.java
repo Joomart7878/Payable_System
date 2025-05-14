7a45da56ea7f5fee22bca8f9d19fce842a7f9038
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-14 08:36:54 +0600

    constant of bigO

---
+++ src/main/java/bigo/Const.java
@@ -0,0 +1,8 @@
+package bigo;
+
+public class Const {
+    // O(10 - always takes the same time regardless of array size
+    public static int getElement(int[] array, int index) {
+        return array[index];
+    }
+}
