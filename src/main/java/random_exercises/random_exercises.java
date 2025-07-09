dfd4d8d574305e482ce73c8525e9e7999030a9d8
Joomart Abdykulov <joomart.abd@gmail.com>
2025-07-09 23:09:16 +0600

    exercise 1 operations

---
+++ src/main/java/random_exercises/random_exercises.java
@@ -0,0 +1,26 @@
+package random_exercises;
+
+import java.awt.*;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class random_exercises {
+
+    // Exercise 1 Operations
+    public static String calculateOperations(int a, int b) {
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+
+        return String.format("Sum: %d, Difference: %d, Product: %d", sum, difference, product);
+    }
+
+
+
+    }
+
+
+
+
