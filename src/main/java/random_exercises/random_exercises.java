9f7da7eca9b66e26c8db45288070fbb6a2cb3c22
Joomart Abdykulov <joomart.abd@gmail.com>
2025-07-11 23:09:06 +0600

    exercise 4 array processing

--- src/main/java/random_exercises/random_exercises.java
+++ src/main/java/random_exercises/random_exercises.java
@@ -34,6 +34,25 @@ public class random_exercises {
         return new String(chars);
     }
 
+    // Exercise 4 Array Processing
+    public static Integer findSecondLargest(int[] array) {
+        if (array == null || array.length < 2) return null;
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : array) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        return secondLargest == Integer.MIN_VALUE ? null : secondLargest;
+    }
+
+
 
     }
 
