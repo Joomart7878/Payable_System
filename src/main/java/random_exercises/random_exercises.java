77f7d6055f76259d7b89464dc7b14194d838738d
Joomart Abdykulov <joomart.abd@gmail.com>
2025-07-15 23:11:54 +0600

    exercise 8 generics

--- src/main/java/random_exercises/random_exercises.java
+++ src/main/java/random_exercises/random_exercises.java
@@ -146,7 +146,39 @@ public class random_exercises {
     }
 
 
+    // Exercise 8 Basic Generics
+    static class Pair<T, U> {
+        private T first;
+        private U second;
 
+        public Pair(T first, U second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public T getFirst() {return first;}
+        public U getSecond() {return second;}
+        public void setFirst(T first) {this.first = first;}
+        public void setSecond(U secondt) {this.second = second;}
+
+        @Override
+        public String toString() {
+            return String.format("Pair{first=%s, second=%s", first, second);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Pair<?, ?> pair = (Pair<?, ?>) obj;
+            return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(first, second);
+        }
+    }
 
 
     }
