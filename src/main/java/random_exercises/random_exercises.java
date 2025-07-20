ddd736309abf81a31ec255805097d947570cc2b9
Joomart Abdykulov <joomart.abd@gmail.com>
2025-07-20 23:15:05 +0600

    exercise 14 stream api with colletcors

--- src/main/java/random_exercises/random_exercises.java
+++ src/main/java/random_exercises/random_exercises.java
@@ -245,6 +245,35 @@ public class random_exercises {
                 .collect(Collectors.toList());
     }
 
+    // Exercise 14 Stream API - Advanced with Collectors
+    static class Person {
+        private final String name;
+        private final int age;
+        private final String city;
+
+        public Person(String name, int age, String city) {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+        }
+
+        public String getName() {return name;}
+        public int getAge() {return age;}
+        public String getCity() {return city;}
+
+        @Override
+        public String toString() {
+            return String.format("Person{name='%s, age=%d, city=%s'}", name, age, city);
+        }
+
+        public static Map<String, Double> calculateAverageAgeByCity(List<Person> people) {
+            return people.stream()
+                    .collect(Collectors.groupingBy(
+                            Person::getCity,
+                            Collectors.averagingInt(Person::getAge)
+                    ));
+        }
+    }
 
 
 
